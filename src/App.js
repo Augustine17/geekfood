e3c4343eab9c592970cf6b0886e68d376539a95b
Augustine71 <amenezes927@gmail.com>
2024-03-07 21:32:39 +0530

    geekffood

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { Navbar } from './Components/Navbar/Navbar';
+import { HeroBanner } from './Components/HeroBanner/HeroBanner';
+import { Content } from './Components/Content/Content';
+import { CustomerTestimonials } from './Components/CustomerTestimonials/CustomerTestimonials';
+import { Footer } from './Components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <HeroBanner />
+      <Content />
+      <CustomerTestimonials />
+      <Footer />
+    </>
   );
 }
 
